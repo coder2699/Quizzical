1cefbdbf57b49d54209161502a56034b7566e14e
coder2699 <pranshu0414@gmail.com>
2024-08-21 01:54:15 +0530

    added service class

---
+++ src/main/java/com/quizzical/quizzical/Service/QuestionsService.java
@@ -0,0 +1,15 @@
+package com.quizzical.quizzical.Service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.quizzical.quizzical.Questions;
+
+@Service
+public class QuestionsService {
+
+    public List<Questions> getAllQuestions() {
+        throw new UnsupportedOperationException("Unimplemented method 'getAllQuestions'");
+    }
+}
